a20a301f81c735eade93925f3e873c47269c5c17
StarBunny007 <pramodpenumala123@gmail.com>
2024-09-12 04:33:41 +0530

    80900

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-zi3+ksd7&h!jd*qrw7*4t30amm-v0)7oy+a*+ke$et%3#fqsoa'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'iith-mae-cws.onrender.com',
