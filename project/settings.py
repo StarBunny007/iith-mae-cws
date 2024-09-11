434dbd18d1bae27dac4f64835febd3dacf14c56a
StarBunny007 <pramodpenumala123@gmail.com>
2024-09-12 03:34:03 +0530

    sji

--- project/settings.py
+++ project/settings.py
@@ -134,7 +134,7 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Ensure this line is present
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
